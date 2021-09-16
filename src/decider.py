e912dfe1e1322c1d698043137b121b4368333505
pcygan44 <p.cygan44@gmail.com>
2021-09-16 15:08:06 +0100

    Commit 2

--- src/decider.py
+++ src/decider.py
@@ -2,9 +2,23 @@
 
 
 def preferences(task1, task2):
-    if task1.description == "Wash the Dishes" and task2.description == "Cook Dinner":
+    if task1.description == "Wash Dishes" and task2.description == "Cook Dinner":
         return task1.description
     if task1.description == "Cook Dinner" and task2.description == "Clean Windows":
         return task1.description
-    if task1.description == "Clean Windows" and task2.description == "Wash the Dishes":
+    if task1.description == "Clean Windows" and task2.description == "Wash Dishes":
+        return task1.description
+    if task1.description == "Wash Dishes" and task2.description == "Wash Clothes":
+        return task1.description
+    if task1.description == "Cook Dinner" and task2.description == "Do Ironing":
+        return task1.description
+    if task1.description == "Clean Windows" and task2.description == "Do Ironing":
+        return task1.description
+    if task1.description == "Do Ironing" and task2.description == "Wash Clothes":
+        return task1.description
+    if task1.description == "Do Ironing" and task2.description == "Wash Dishes":
+        return task1.description
+    if task1.description == "Wash Clothes" and task2.description == "Cook Dinner":
+        return task1.description
+    if task1.description == "Wash Clothes" and task2.description == "Clean Windows":
         return task1.description
--- tests/test_task.py
+++ tests/test_task.py
@@ -5,8 +5,10 @@ from src.decider import preferences
 class TestTask(unittest.TestCase):
     def setUp(self):
         self.clean = Task("Clean Windows", 30)
-        self.wash = Task("Wash the Dishes",40)
+        self.wash = Task("Wash Dishes",40)
         self.cook = Task("Cook Dinner", 15)
+        self.ironing = Task("Do Ironing", 45)
+        self.clothes = Task("Wash Clothes", 70)
 
     def test_name(self):
         self.assertEqual("Clean Windows", self.clean.description)
@@ -19,4 +21,14 @@ class TestTask(unittest.TestCase):
         # task1 = Task("Wash the Dishes",40)
         # task2 = Task("Cook Dinner", 15)
         # Task.preferences(self.cook, self.wash)
-        self.assertEqual("Wash the Dishes",preferences(self.wash, self.cook)) 
+        self.assertEqual("Wash Dishes",preferences(self.wash, self.cook)) 
+        self.assertEqual("Cook Dinner",preferences(self.cook, self.clean)) 
+        self.assertEqual("Clean Windows", preferences(self.clean, self.wash))
+        self.assertEqual("Wash Dishes", preferences(self.wash, self.clothes))
+        self.assertEqual("Cook Dinner", preferences(self.cook, self.ironing))
+        self.assertEqual("Clean Windows",preferences(self.clean, self.ironing)) 
+        self.assertEqual("Do Ironing",preferences(self.ironing, self.wash)) 
+        self.assertEqual("Do Ironing" , preferences(self.ironing, self.wash))
+        self.assertEqual("Wash Clothes", preferences(self.clothes, self.cook))
+        self.assertEqual("Wash Clothes", preferences(self.clothes, self.clean))
+    
